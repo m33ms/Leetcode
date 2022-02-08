080827f0849f8f5d52edae20b8006b9c334e71e1
mimi schirm <mimischirm@gmail.com>
2022-02-08 09:52:03 -0800

    solve 643

--- src/MaxAverageSubarrayI.java
+++ src/MaxAverageSubarrayI.java
@@ -10,14 +10,33 @@ Output: 12.75000
 Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
  */
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class MaxAverageSubarrayI {
     static double findMaxAverage(int[] nums, int k) {
+        double max = Double.NEGATIVE_INFINITY;
+        double avg = Double.NEGATIVE_INFINITY;
+        double sum = 0;
 
+        Queue<Integer> list = new LinkedList<>();
+
+        for(int i=0; i<nums.length;i++) {
+            list.add(nums[i]);
+            sum += nums[i];
+
+            if (list.size() == k) {
+                avg = sum / k;
+                max = Math.max(max, avg);
+                sum -= list.poll();
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 12, -5, -6, 50, 3};
         int k = 4;
-        
+        System.out.println(findMaxAverage(nums, k));
     }
 }
