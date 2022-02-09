ccf15ba630f271827eb21268ef74a0264911f1d0
mimi schirm <mimischirm@gmail.com>
2022-02-08 21:11:48 -0800

    add 1961. Check If String Is a Prefix of Array

---
+++ src/IsStringPrefixArray.java
@@ -0,0 +1,22 @@
+/*
+1961. Check If String Is a Prefix of Array
+https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/
+
+Given a string s and an array of strings words, determine whether s is a prefix string of words.
+A string s is a prefix string of words if s can be made by concatenating the first
+k strings in words for some positive k no larger than words.length.
+Return true if s is a prefix string of words, or false otherwise.
+
+Input: s = "iloveleetcode", words = ["i","love","leetcode","apples"]
+Output: true
+ */
+
+public class IsStringPrefixArray {
+   static boolean isPrefixString(String s, String[] words) {
+
+   }
+
+    public static void main(String[] args) {
+        
+    }
+}
