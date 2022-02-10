ec2b45bc9db9ae779ce862142cc4060137795720
mimi schirm <mimischirm@gmail.com>
2022-02-10 12:47:54 -0800

    solve 21

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 21. Merge Two Sorted Lists
 https://leetcode.com/problems/merge-two-sorted-lists/
@@ -11,22 +13,51 @@ Return the head of the merged linked list.
 Input: list1 = [1,2,4], list2 = [1,3,4]
 Output: [1,1,2,3,4,4]
 
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
  */
+
+import java.util.Arrays;
+import java.util.List;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 
 public class MergeTwoSortedLists {
 
     static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode tempNode = new ListNode(0);
+        ListNode currentNode = tempNode;
 
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                currentNode.next = list1;
+                list1 = list1.next;
+            } else {
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+            currentNode = currentNode.next;
+        }
+
+        //if list2 is null but not list1
+        if(list1 != null) {
+            currentNode.next = list1;
+            list1 = list1.next;
+        }
+
+        if(list2 != null) {
+            currentNode.next = list2;
+            list2 = list2.next;
+        }
+
+        return tempNode.next;
     }
 
     public static void main(String[] args) {
-        
+
     }
 }
