c411ec0d9e7bd6c98a17fb103efcef367be47b9d
mimi schirm <mimischirm@gmail.com>
2022-02-10 10:17:05 -0800

    solve 204

--- src/CountPrimes.java
+++ src/CountPrimes.java
@@ -8,10 +8,31 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 
 public class CountPrimes {
     static int countPrimes(int n) {
+        //sieve of Eratosthenes
+        int count = 0;
+        boolean[] primes = new boolean[n];
 
+        //i=2 because 2 is first prime number
+        //multiples of i will not be prime
+        for(int i=2; i*i < primes.length; i++) {
+            if(!primes[i]) {
+                for(int j=i; j*i<primes.length; j++) {
+                    //boolean by default is false, so set multiples of i to true
+                    primes[i*j] = true;
+                }
+            }
+        }
+
+        for(int i=2; i<primes.length; i++){
+            if(!primes[i]) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        
+        int n = 10;
+        System.out.println("count: " + countPrimes(n));
     }
 }
