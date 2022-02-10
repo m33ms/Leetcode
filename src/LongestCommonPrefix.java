1f75a5c3a64d92a8eeb917b3a81e9ce62a351bba
mimi schirm <mimischirm@gmail.com>
2022-02-10 08:28:49 -0800

    add 14. Longest Common Prefix

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+/*
+14. Longest Common Prefix
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+https://leetcode.com/problems/longest-common-prefix/
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+ */
+public class LongestCommonPrefix {
+    static String longestCommonPrefix(String[] strs) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
