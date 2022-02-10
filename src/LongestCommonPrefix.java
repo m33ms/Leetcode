c07990159371a06d37c049f3b1e34a45bd410484
mimi schirm <mimischirm@gmail.com>
2022-02-10 08:59:09 -0800

    solve 14

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -9,10 +9,23 @@ Output: "fl"
  */
 public class LongestCommonPrefix {
     static String longestCommonPrefix(String[] strs) {
+        //prefix = flower
+        String prefix = strs[0];
 
+        if(strs.length == 0) return "";
+        //iterate starting from the second element in the array
+        for(int i=1; i<strs.length; i++) {
+            while(strs[i].indexOf(prefix) != 0) {
+                //reduce length of prefix to until prefix found
+                prefix = prefix.substring(0, prefix.length()-1);
+            }
+        }
+        return prefix;
     }
 
     public static void main(String[] args) {
-        
+        String[] strs = {"flower", "flow", "flight"};
+
+        System.out.println(longestCommonPrefix(strs));
     }
 }
