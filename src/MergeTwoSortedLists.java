7542579688b2f895e3fe184f4152beb5d6d4a4e6
mimi schirm <mimischirm@gmail.com>
2022-02-10 12:27:02 -0800

    add 21. Merge Two Sorted Lists

---
+++ src/MergeTwoSortedLists.java
@@ -0,0 +1,32 @@
+/*
+21. Merge Two Sorted Lists
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list.
+The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class MergeTwoSortedLists {
+
+    static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
