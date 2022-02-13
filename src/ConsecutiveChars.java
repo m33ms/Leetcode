3729b22767be9ff071b76f906af2417a86cd2c18
mimi schirm <mimischirm@gmail.com>
2022-02-13 13:36:05 -0800

    add 1446. Consecutive Characters

---
+++ src/ConsecutiveChars.java
@@ -0,0 +1,21 @@
+/*
+1446. Consecutive Characters
+https://leetcode.com/problems/consecutive-characters/
+The power of the string is the maximum length of a non-empty
+substring that contains only one unique character.
+
+Given a string s, return the power of s.
+Input: s = "leetcode"
+Output: 2
+The substring "ee" is of length 2 with the character 'e' only.
+ */
+
+public class ConsecutiveChars {
+    static int maxPower(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
