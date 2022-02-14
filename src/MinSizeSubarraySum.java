ed9a560709029efe62e09e54b6e528dee15ff87e
mimi schirm <mimischirm@gmail.com>
2022-02-14 11:46:10 -0800

    add 209. Minimum Size Subarray Sum

---
+++ src/MinSizeSubarraySum.java
@@ -0,0 +1,22 @@
+/*
+209. Minimum Size Subarray Sum
+https://leetcode.com/problems/minimum-size-subarray-sum/
+Given an array of positive integers nums and a positive integer target,
+return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr]
+of which the sum is greater than or equal to target. If there is no such subarray,
+return 0 instead.
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+The subarray [4,3] has the minimal length under the problem constraint.
+ */
+
+public class MinSizeSubarraySum {
+    static int minSubArrayLen(int target, int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
