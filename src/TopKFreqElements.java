fd16a9320ec694ca96d2415bb55483793717d893
mimi schirm <mimischirm@gmail.com>
2022-02-14 12:18:51 -0800

    add 347. Top K Frequent Elements

---
+++ src/TopKFreqElements.java
@@ -0,0 +1,20 @@
+/*
+347. Top K Frequent Elements
+https://leetcode.com/problems/top-k-frequent-elements/
+
+Given an integer array nums and an integer k,
+return the k most frequent elements. You may return the answer in any order.
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+ */
+
+public class TopKFreqElements {
+    static int[] topKFrequent(int[] nums, int k) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
