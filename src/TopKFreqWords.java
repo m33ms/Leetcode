c61725af6cd2a09ad5b9e809604d33657d6ec5ae
mimi schirm <mimischirm@gmail.com>
2022-02-15 17:49:25 -0800

    add 692. Top K Frequent Words

---
+++ src/TopKFreqWords.java
@@ -0,0 +1,22 @@
+/*
+692. Top K Frequent Words
+https://leetcode.com/problems/top-k-frequent-words/
+Given an array of strings words and an integer k, return the k most frequent strings.
+
+Return the answer sorted by the frequency from highest to lowest.
+Sort the words with the same frequency by their lexicographical order.
+Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+Output: ["i","love"]
+ */
+
+import java.util.List;
+
+public class TopKFreqWords {
+    static List<String> topKFrequent(String[] words, int k) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
