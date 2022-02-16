867b3e2513c92b3efd27c78cfca031fabf91fa60
mimi schirm <mimischirm@gmail.com>
2022-02-15 16:34:17 -0800

    add 451. Sort Characters By Frequency

---
+++ src/SortCharsByFrequency.java
@@ -0,0 +1,20 @@
+/*
+451. Sort Characters By Frequency
+https://leetcode.com/problems/sort-characters-by-frequency/
+Given a string s, sort it in decreasing order based on the frequency of the characters.
+The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+Input: s = "tree"
+Output: "eert"
+ */
+
+public class SortCharsByFrequency {
+    static String frequencySort(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
--- src/TopKFreqElements.java
+++ src/TopKFreqElements.java
@@ -9,12 +9,40 @@ Input: nums = [1,1,1,2,2,3], k = 2
 Output: [1,2]
  */
 
+import java.util.*;
+
 public class TopKFreqElements {
     static int[] topKFrequent(int[] nums, int k) {
+        //create an arraylist to store items from hashmap
+        List<Integer> list = new ArrayList<>();
 
+        //create hashmap to store and count frequency of integers
+        //add values from nums array to map
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        //iterate through newly created hashmap
+        //if key values is >= k, add to list
+        for(int key: map.keySet()) {
+            if(map.get(key) >= k) {
+                list.add(key);
+            }
+        }
+        //create array
+        //add values from arraylist to array
+        int index = 0;
+        int[] result = new int[list.size()];
+        for(Integer i: list) {
+            result[index++] = i;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums, k)));
     }
 }
