c2af335a7a34070a6caa57eb4612cc0f7891191a
mimi schirm <mimischirm@gmail.com>
2022-02-15 16:45:34 -0800

    solve 451

--- src/SortCharsByFrequency.java
+++ src/SortCharsByFrequency.java
@@ -9,12 +9,40 @@ Input: s = "tree"
 Output: "eert"
  */
 
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 public class SortCharsByFrequency {
     static String frequencySort(String s) {
+        //create hashmap to count character frequency
+        HashMap<Character, Integer> map = new HashMap<>();
+        //iterate through chars in string and add to map
+        for(char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
 
+        //create priority queue to sort frequency from high to low
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
+        //add all chars to priority queue
+        maxHeap.addAll(map.keySet());
+
+        //create string builder and add chars
+        StringBuilder result = new StringBuilder();
+        while(!maxHeap.isEmpty()) {
+            char current = maxHeap.remove();
+            //iterate through hashmap to get frequency of char
+            for(int i=0; i<map.get(current); i++) {
+                result.append(current);
+            }
+        }
+        return result.toString();
     }
 
     public static void main(String[] args) {
-        
+        String str = "tree";
+        String s = "cccaaa";
+
+        System.out.println(frequencySort(str));
+        System.out.println(frequencySort(s));
     }
 }
