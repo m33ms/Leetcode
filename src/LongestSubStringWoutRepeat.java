3cb16d064fdf77df3c259aac4e3aaccddc7e2c65
mimi schirm <mimischirm@gmail.com>
2022-02-15 19:27:47 -0800

    sove 3

--- src/LongestSubStringWoutRepeat.java
+++ src/LongestSubStringWoutRepeat.java
@@ -6,12 +6,34 @@ Input: s = "abcabcbb"
 Output: 3
  */
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubStringWoutRepeat {
     static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int start = 0, end = 0;
+        Set<Character> set = new HashSet<>();
 
+        while(end < s.length()) {
+            char currentChar = s.charAt(end);
+            if(!set.contains(currentChar)) {
+                set.add(currentChar);
+                end++;
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(start));
+                start++;
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
+        String s = "abcabcbb";
+        String s1 = "bbbbbbb";
 
+        System.out.println(lengthOfLongestSubstring(s));
+        System.out.println(lengthOfLongestSubstring(s1));
     }
 }
